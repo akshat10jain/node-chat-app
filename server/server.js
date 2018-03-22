dc99e8f4af824e8e09ca5eb0e0f22dad0b46f76f
Akshat jain <akshat.jain100896@gmail.com>
2018-03-22 17:02:05 +0530

    adding console

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const publicPath=path.join(__dirname,'../public')
 const port =process.env.PORT || 3000;
 var app=express();
 app.use(express.static(publicPath));
-
+console.log('hello');
 app.listen(3000,()=>{
     console.log(`server is up on ${port}`);
 })
