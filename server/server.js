8d4e20ea29b51949707a6553fd2b9859ba0d2fb4
Akshat jain <akshat.jain100896@gmail.com>
2018-03-25 09:33:19 +0530

    greet new user and aler others

--- server/server.js
+++ server/server.js
@@ -17,16 +17,38 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
  console.log('new user connected');
 
- 
+ // socket .emit  from admin text welcome to chat app
+  socket.emit('newMessage',{
+      from:'Admin',
+      text:'Welcome to chat app',
+      createdAt:new Date().getTime()
+  })
+
+ //socket .broadcast .emit from admin text new user joined
+ socket.broadcast.emit('newMessage',{
+     form:'Admin',
+     text:'New user joined',
+     createdAt:new Date().getTime()
+ })
 
 socket.on('createMessage',(message)=>{
   console.log('createMessage', message);
+
+  // every body see the message including me
   io.emit('newMessage',{
     from:message.from,
     text:message.text,
     createdAt:new Date().getTime()
   })
-})
+
+// we will not see the message we send but every body else
+
+//  socket.broadcast.emit('newMessage', {
+//     from:message.from,
+//         text:message.text,
+//         createdAt:new Date().getTime()
+//  });
+});
    
 
  socket.on('disconnect',()=>{
