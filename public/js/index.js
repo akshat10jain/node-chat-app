3981dc45965907f4d5efbcfaa7863a300e87e539
Akshat jain <akshat.jain100896@gmail.com>
2018-03-25 09:08:21 +0530

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,7 @@ var socket=   io();
 
  socket.on('connect',function (){
      console.log('connected to server');
-
-socket.emit('createMessage',{
-    from:'jfh'
-
 })
-
- })
-
  socket.on('disconnect',function (){
     console.log('disconneced from server');
  })
--- server/server.js
+++ server/server.js
@@ -17,14 +17,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
  console.log('new user connected');
 
- socket.emit('newMessage' , {
-     from: 'aksha',
-     text:'hello',
-     createdAt:1234
-    });
+ 
 
 socket.on('createMessage',(message)=>{
   console.log('createMessage', message);
+  io.emit('newMessage',{
+    from:message.from,
+    text:message.text,
+    createdAt:new Date().getTime()
+  })
 })
    
 
