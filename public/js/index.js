dad9541a0090324d06bef1845cef41eb523f59ac
Akshat jain <akshat.jain100896@gmail.com>
2018-03-29 11:37:53 +0530

    changes made to group chat deleting join chat

---
+++ public/js/index.js
@@ -0,0 +1,73 @@
+var socket = io();
+
+function scrollToBottom() {
+    //selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+
+socket.on('connect', function () {
+    console.log('connected to server');
+    // var params = jQuery.deparam(window.location.search);
+
+})
+
+socket.on('disconnect', function () {
+    console.log('disconneced from server');
+})
+
+socket.on('newMessage', function (data) {
+
+    console.log('disconneced from server');
+    
+    var formattedTime = moment(data.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: data.text,
+        from: data.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+    scrollToBottom();
+
+    // var formattedTime=moment(data.createdAt).format('h:mm a');
+
+    //  var li=jQuery('<li></li>');
+    //  li.text(`${data.from} ${formattedTime} : ${data.text}`);
+
+    //  jQuery('#messages').append(li);
+})
+
+
+//acknowledge sent from server to client
+//  socket.emit('createMessage', {
+//     from:'froank',
+//     text:'hi'
+// },function(data) {
+//     console.log('got it',data);
+// });
+
+
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    //  var params = jQuery.deparam(window.location.search);
+    socket.emit('createMessage', {
+        from:'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+        jQuery('[name=message]').val('')
+    })
+})
--- server/server.js
+++ server/server.js
@@ -1,51 +1,78 @@
-const path=require('path');
-const http=require('http');
-const express=require('express');
-const socketIO=require('socket.io');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const {generateMessage}=require('./utils/message');
+const { generateMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
 
-const publicPath=path.join(__dirname,'../public')
-const port =process.env.PORT || 3000;
-var app=express();
+const publicPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+var app = express();
 
-var server=http.createServer(app);
+var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 //console.log('jgjhg')
 
-io.on('connection',(socket)=>{
- console.log('new user connected');
+io.on('connection', (socket) => {
+  console.log('new user connected');
 
- // socket .emit  from admin text welcome to chat app
-  socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+  // socket .emit  from admin text welcome to chat app
 
- //socket .broadcast .emit from admin text new user joined
- socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
 
-socket.on('createMessage',(message,callback)=>{
-  console.log('createMessage', message);
+  //socket .broadcast .emit from admin text new user joined
 
-  // every body see the message including me
-  io.emit('newMessage',generateMessage(message.from,message.text))
-  callback();
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-// we will not see the message we send but every body else
 
-//  socket.broadcast.emit('newMessage', {
-//     from:message.from,
-//         text:message.text,
-//         createdAt:new Date().getTime()
-//  });
-});
-   
+  //join
+  // socket.on('join' , (params,callback)=>{
 
- socket.on('disconnect',()=>{
+  //   if(!isRealString(params.name) || !isRealString(params.room)){
+  //     callback('Name and room are required');
+  //   }
+
+  //   socket.join(params.room);
+
+  //io.emit-> io.to.emit
+  //socket.broadcast.emit->socket.broadcast.to.emit
+  //socket.emit
+
+  //   socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+  //   socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined`));
+
+
+  //   callback();
+
+  // })
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+
+    // every body see the message including me
+    io.emit('newMessage', generateMessage(message.from, message.text))
+    callback();
+
+
+    // we will not see the message we send but every body else
+
+
+    //  socket.broadcast.emit('newMessage', {
+    //     from:message.from,
+    //         text:message.text,
+    //         createdAt:new Date().getTime()
+    //  });
+  });
+
+
+  socket.on('disconnect', () => {
     console.log('user was disconnected');
- })
+  })
 });
 
-server.listen(port,()=>{
-    console.log(`server is up on ${port}`);
+server.listen(port, () => {
+  console.log(`server is up on ${port}`);
 })
